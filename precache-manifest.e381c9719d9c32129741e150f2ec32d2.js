5c18f78af6bbc151181eefe1353ae7beb86b20a0
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-07-09 00:12:22 +0100

    Updates

---
+++ precache-manifest.e381c9719d9c32129741e150f2ec32d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979a5e62cb8783864fe0d87f8c0e34fe",
+    "url": "/job-listings/index.html"
+  },
+  {
+    "revision": "ba1b987fbdb80581fd7f",
+    "url": "/job-listings/static/css/main.2c281ac7.chunk.css"
+  },
+  {
+    "revision": "9723492d84949f9d755e",
+    "url": "/job-listings/static/js/2.b8840fd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-listings/static/js/2.b8840fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1b987fbdb80581fd7f",
+    "url": "/job-listings/static/js/main.1345e75f.chunk.js"
+  },
+  {
+    "revision": "32d5980de96642a4c809",
+    "url": "/job-listings/static/js/runtime-main.960a6a11.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/job-listings/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/job-listings/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/job-listings/static/media/icon-remove.ae67c678.svg"
+  }
+]);
